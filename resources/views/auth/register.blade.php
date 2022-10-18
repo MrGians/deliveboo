e243362379c2f3efd2a645add6ad603e6a4555a1
MrGians <gianlumura97@gmail.com>
2022-10-18 17:25:24 +0200

    fix form for user and restaurant registration

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
-
+                        {{-- Name --}}
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
 
@@ -24,7 +24,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        {{-- Email --}}
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
@@ -38,7 +38,77 @@
                                 @enderror
                             </div>
                         </div>
+                        {{-- Restaurant Name --}}
+                        <div class="form-group row">
+                            <label for="restaurant_name" class="col-md-4 col-form-label text-md-right">Nome Ristorante</label>
 
+                            <div class="col-md-6">
+                                <input id="restaurant_name" type="text" class="form-control @error('restaurant_name') is-invalid @enderror" name="restaurant_name" value="{{ old('restaurant_name') }}" required>
+
+                                @error('restaurant_name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- Restaurant Description --}}
+                        <div class="form-group row">
+                            <label for="restaurant_description" class="col-md-4 col-form-label text-md-right">Descrizione Ristorante</label>
+
+                            <div class="col-md-6">
+                                <textarea id="restaurant_description" class="form-control @error('restaurant_description') is-invalid @enderror" name="restaurant_description" required>{{ old('restaurant_description') }}</textarea>
+
+                                @error('restaurant_description')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- Restaurant Logo --}}
+                        <div class="form-group row">
+                            <label for="restaurant_logo" class="col-md-4 col-form-label text-md-right">Logo Ristorante</label>
+
+                            <div class="col-md-6">
+                                <input id="restaurant_logo" type="file" class="form-control @error('restaurant_logo') is-invalid @enderror" name="restaurant_logo" required>
+
+                                @error('restaurant_logo')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- Restaurant Address --}}
+                        <div class="form-group row">
+                            <label for="restaurant_address" class="col-md-4 col-form-label text-md-right">Indirizzo Ristorante</label>
+
+                            <div class="col-md-6">
+                                <input id="restaurant_address" type="text" class="form-control @error('restaurant_address') is-invalid @enderror" name="restaurant_address" value="{{ old('restaurant_address') }}" required>
+
+                                @error('restaurant_address')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- Restaurant P.IVA --}}
+                        <div class="form-group row">
+                            <label for="p_iva" class="col-md-4 col-form-label text-md-right">Partita IVA</label>
+
+                            <div class="col-md-6">
+                                <input id="p_iva" type="p_iva" class="form-control @error('p_iva') is-invalid @enderror" name="p_iva" value="{{ old('p_iva') }}" required>
+
+                                @error('p_iva')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                        {{-- Password --}}
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
@@ -52,7 +122,7 @@
                                 @enderror
                             </div>
                         </div>
-
+                        {{-- Confirm Password --}}
                         <div class="form-group row">
                             <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
 
