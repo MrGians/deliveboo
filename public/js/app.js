094d24b226a98d98feae86a4f2dbb0ace14242c1
Gabridg <gabri99.del@gmail.com>
2022-10-19 11:01:55 +0200

    fix err on Seeder

--- public/js/app.js
+++ public/js/app.js
@@ -37366,8 +37366,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Laravel\deliveboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Laravel\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laravel\deliveboo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laravel\deliveboo\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
