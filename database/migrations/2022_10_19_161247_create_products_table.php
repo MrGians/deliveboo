baf939d8bf63ca8bf169ad3b52cef6b47be65f7e
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-10-19 18:19:14 +0200

    Create products miration

---
+++ database/migrations/2022_10_19_161247_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('restaurant_id');
+            $table->foreign('restaurant_id')->references('id')->on('restaurants')->onDelete('cascade');
+
+            $table->string('name');
+            $table->smallInt('price');
+            $table->string('description');
+            $table->string('image');
+            $table->boolean('is_visible')->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
