02196a3acb271407490b4c2494535c1681630c8f
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-10-25 19:22:27 +0200

    added media query searchbar

--- resources/js/components/BaseSearchBar.vue
+++ resources/js/components/BaseSearchBar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    
         <input type="text" class="d-flex align-items-center justify-content-center" id="searchbar" :placeholder="placeholder">
-    </div>
+    
 
 </template>
 
@@ -18,15 +18,44 @@ export default {
 #searchbar {
 
     display: inline-block;
-    width: 450px;
+    width: 50%;
     height: 70px;
     border: 3px solid transparent;
     padding: 12px 20px;
     border-radius: 50px;
     box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-    font-size: 20px;
+    font-size: 1.3rem;
     
 }
+
+@media (min-width: 576px) {
+    #searchbar {
+        height: 40px;
+        font-size: 0.6rem;
+    }
+}
+
+@media (min-width: 768px) {
+    #searchbar {
+        height: 50px;
+        font-size: 0.8rem;
+    }
+}
+
+@media (min-width: 992px) {
+    #searchbar {
+        height: 60px;
+        font-size: 1rem;
+    }
+}
+
+@media (min-width: 1200px) {
+    #searchbar {
+        height: 70px;
+        font-size: 1.3rem;
+    }
+}
+
 
 #searchbar:focus {
     outline: 0;
