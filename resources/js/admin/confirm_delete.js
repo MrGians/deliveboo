248f2388f83363eddcaddc6040df5fe7ba5300dd
MrGians <gianlumura97@gmail.com>
2022-10-25 17:04:45 +0200

    fix confirm_delete logic for custom modal

--- resources/js/admin/confirm_delete.js
+++ resources/js/admin/confirm_delete.js
@@ -1,17 +1,22 @@
 const formsToDelete = document.querySelectorAll(".delete-form");
 
+const customModal = document.getElementById("myModal");
+const modalConfirmBtn = document.getElementById("delete-confirm");
+const modalBackBtn = document.getElementById("delete-go-back");
+const modalContent = document.querySelector("#myModal p");
+
 formsToDelete.forEach((form) => {
     form.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let target = 'questo';
+        // switch case to set correct message
+        let target = "questo";
         let element = "elemento";
         const cls = form.classList;
 
         switch (true) {
             case cls.contains("delete-product"):
                 element = "Prodotto";
-                console.log(element);
                 break;
             case cls.contains("delete-null"):
                 element = "";
@@ -21,7 +26,20 @@ formsToDelete.forEach((form) => {
                 break;
         }
 
-        const confirmation = confirm(`Vuoi cancellare definitivamente ${target} ${element}? L'azione è irreversibile`);
-        if (confirmation) form.submit();
+        // Modal message to display
+        let modalMessage = `Vuoi cancellare definitivamente ${target} ${element}? L'azione è irreversibile`;
+        modalContent.innerText = modalMessage;
+
+        // Show modal
+        customModal.style.display = "block";
+        // If click confirm, hide modal & submit form
+        modalConfirmBtn.addEventListener("click", () => {
+            customModal.style.display = "none";
+            form.submit();
+        });
+        // If click go back hide modal
+        modalBackBtn.addEventListener("click", () => {
+            customModal.style.display = "none";
+        });
     });
 });
