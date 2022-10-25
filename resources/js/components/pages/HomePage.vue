d3fdaa58a6ed1ec27148f7deca9198a93c8356bb
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-10-25 19:23:29 +0200

    fixed content and added media query to homepage

--- resources/js/components/pages/HomePage.vue
+++ resources/js/components/pages/HomePage.vue
@@ -1,21 +1,24 @@
 <template>
     <div id="home-page">
-        <header>
-            <div id="background-food">
-                <BaseHeader></BaseHeader>
-                <div id="searchbar-container" class="container padding d-flex align-items-center justify-content-center">
-                    <BaseSearchBar :placeholder="placeholder"></BaseSearchBar>
-                </div>
+       
+        <div id="background-food">
+                
+            <div id="searchbar-container" class="container padding d-flex align-items-center justify-content-center">
+                <BaseSearchBar :placeholder="placeholder"></BaseSearchBar>
             </div>
-        </header>
+        </div>
+        
+
 
         <div id="home-page-content">
             <div id="title" class="text-center">
                 <h1>Le cucine più amate</h1>
             </div>
 
-            <div id="filters-cards">
-                <BaseCard></BaseCard>
+            <div id="filters-cards" class="container">
+                <div class="row">
+                    <BaseCard v-for="index in 16" :key="index"></BaseCard>
+                </div>
             </div>
         </div>
     </div>
@@ -44,24 +47,54 @@ export default {
 
 #title {
     color: #fc5958;
+    margin-bottom: 100px;
 }
 
-header {
-    background-image: url('/img/jumbotron.png');
-    background-repeat: no-repeat;
-    background-position: center;
-    background-size: cover;
-    margin-bottom: 50px;
+#home-page{
+    min-height: 200px;
+    margin-bottom: 150px;
 }
+
+
 
 #background-food {
     
     
     background-image: url('/img/burger_sandwich_PNG4135.png');
     background-repeat: no-repeat;
-    background-position: right;
-    background-size: 55%;
+    background-position: center right;
+    background-size: 45%;
+    margin-bottom: 100px;
 }
+
+#home-page-content {
+    min-height: 800px;
+}
+
+@media (min-width: 576px) {
+    .container {
+        max-width: 540px;
+    }
+}
+
+@media (min-width: 768px) {
+    .container {
+        max-width: 720px;
+    }
+}
+
+@media (min-width: 992px) {
+    .container {
+        max-width: 960px;
+    }
+}
+
+@media (min-width: 1200px) {
+    .container {
+        max-width: 1140px;
+    }
+}
+
 
 
 </style>
