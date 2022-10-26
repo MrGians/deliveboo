8bf9a3a87741278ca285d0ec36c0ea63b415d33b
MrGians <gianlumura97@gmail.com>
2022-10-26 20:15:55 +0200

    Rebuilded BaseSearchBar

--- resources/js/components/BaseSearchBar.vue
+++ resources/js/components/BaseSearchBar.vue
@@ -1,72 +1,87 @@
 <template>
-    
-        <input type="text" class="d-flex align-items-center justify-content-center" id="searchbar" :placeholder="placeholder">
-    
-
+    <div id="base-input">
+        <input
+            type="text"
+            list="datalist"
+            class="shadow rounded-5"
+            :placeholder="placeholder"
+            v-model="value"
+            @keyup="$emit('get-input-value', value)"
+        />
+        <datalist id="datalist">
+            <option v-for="item in listItem" :key="item.id">
+                {{ item.label }}
+            </option>
+        </datalist>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'BaseSearchBar',
+    name: "BaseSearchBar",
+    data() {
+        return {
+            value: "",
+        };
+    },
     props: {
         placeholder: String,
-    }
-}
+        listItem: Array,
+    },
+};
 </script>
 
 <style scoped>
-#searchbar {
-
-    display: inline-block;
+#base-input {
     width: 50%;
-    height: 70px;
+    z-index: 2;
+}
+
+input {
+    display: inline-block;
+    width: 100%;
+    min-width: 200px;
+    height: 30px;
     border: 3px solid transparent;
-    padding: 12px 20px;
-    border-radius: 50px;
-    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
-    font-size: 1.3rem;
-    
+    padding: 1rem;
+    font-size: 0.5rem;
+}
+
+input:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0.2rem rgba(252, 89, 88, 1);
+}
+
+input::placeholder {
+    text-align: center;
+    color: #2eb872;
 }
 
 @media (min-width: 576px) {
-    #searchbar {
+    input {
         height: 40px;
         font-size: 0.6rem;
     }
 }
 
 @media (min-width: 768px) {
-    #searchbar {
+    input {
         height: 50px;
         font-size: 0.8rem;
     }
 }
 
 @media (min-width: 992px) {
-    #searchbar {
+    input {
         height: 60px;
         font-size: 1rem;
     }
 }
 
 @media (min-width: 1200px) {
-    #searchbar {
+    input {
         height: 70px;
         font-size: 1.3rem;
     }
 }
-
-
-#searchbar:focus {
-    outline: 0;
-    box-shadow: 0 0 0 0.2rem rgba(252, 89, 88, 1);
-}
-
-#searchbar::placeholder {
-    text-align: center;
-    color: #2EB872;
-}
-
-
-
 </style>
