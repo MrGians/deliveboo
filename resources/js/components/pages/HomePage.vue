a77bef783b72f04521f6092ef3632d0c908d7c83
MrGians <gianlumura97@gmail.com>
2022-10-26 20:16:09 +0200

    fix homepage

--- resources/js/components/pages/HomePage.vue
+++ resources/js/components/pages/HomePage.vue
@@ -1,98 +1,116 @@
 <template>
     <div id="home-page">
-       
-        <div id="background-food">
-                
-            <div id="searchbar-container" class="container padding d-flex align-items-center justify-content-center">
-                <BaseSearchBar :placeholder="placeholder" @keyup.enter="fetchCategories()"></BaseSearchBar>
-            </div>
+        <div class="search-box">
+            <img src="/img/burger_sandwich_PNG4135.png" alt="" />
+            <BaseSearchBar
+                class="text-center"
+                :placeholder="placeholder"
+                :list-item="categories"
+                @get-input-value="setCategory"
+            />
+
+            <router-link
+                class="search-btn"
+                :to="{
+                    name: 'restaurants',
+                    params: { filter: selectedCategory },
+                }"
+                >Cerca</router-link
+            >
         </div>
-        
 
-
-        <div id="home-page-content">
-            <div id="title" class="text-center">
-                <h1>Le cucine più amate</h1>
-            </div>
-
-            <div id="filters-cards" class="container">
-                <div class="row">
-                    <BaseCard v-for="index in 16" :key="index"></BaseCard>
-                </div>
-            </div>
-        </div>
+        <!-- TODO FOOTER -->
     </div>
 </template>
 
 <script>
-import BaseHeader from '../BaseHeader.vue';
-import BaseSearchBar from '../BaseSearchBar.vue';
-import BaseCard from '../BaseCard.vue';
+import BaseHeader from "../BaseHeader.vue";
+import BaseSearchBar from "../BaseSearchBar.vue";
+import BaseCard from "../BaseCard.vue";
 export default {
     name: "App",
     components: {
-       BaseSearchBar,
-       BaseCard,
-       BaseHeader
+        BaseSearchBar,
+        BaseCard,
+        BaseHeader,
     },
     data() {
         return {
-            placeholder: 'Voglia di qualcosa di buono?',
+            placeholder: "Voglia di qualcosa di buono?",
             categories: [],
             selectedCategory: null,
         };
     },
     methods: {
         fetchCategories() {
-            axios.get('http://127.0.0.1:8000/api/home')
-            .then((res) => {
-                this.categories = res.data;
-            })
-            .catch(() => {
-                // todo
-            }) 
-            .then(() => {
-                // todo
-            })
-        }, 
-        sendCategory() {
-            this.selectedCategory = 2;
-            axios.get('http://127.0.0.1:8000/api/filter-restaurants', this.selectedCategory)
-            .then(() => {
-                // todo data errors
-            })
-            .catch(() => {
-                // todo
-            }) 
-            .then(() => {
-                // todo
-            })
-        }
-    }, 
+            axios
+                .get("http://127.0.0.1:8000/api/home")
+                .then((res) => {
+                    this.categories = res.data;
+                })
+                .catch(() => {
+                    // todo
+                })
+                .then(() => {
+                    // todo
+                });
+        },
+        setCategory(value) {
+            this.selectedCategory = value;
+        },
+    },
     mounted() {
         this.fetchCategories();
     },
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "./../../../sass/front.scss";
 
 #title {
     color: #fc5958;
     margin-bottom: 100px;
 }
 
-#home-page{
+#home-page {
     min-height: 200px;
     margin-bottom: 150px;
+
+    .search-box {
+        position: relative;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        img {
+            position: absolute;
+            bottom: -250%;
+            left: 0;
+            width: 40%;
+            height: auto;
+        }
+        .search-btn {
+            display: inline-block;
+            padding: 0.6rem 1.2rem;
+            border: 2px solid $tertiary;
+            background-color: $tertiary;
+            color: white;
+            border-radius: 30px;
+            margin-left: 1rem;
+            transition: all 0.35s;
+
+            &:hover {
+                background-color: white;
+                color: $tertiary;
+            }
+        }
+    }
 }
 
-
-
 #background-food {
-    
-    
-    background-image: url('/img/burger_sandwich_PNG4135.png');
+    background-image: url("/img/burger_sandwich_PNG4135.png");
     background-repeat: no-repeat;
     background-position: center right;
     background-size: 45%;
@@ -126,7 +144,4 @@ export default {
         max-width: 1140px;
     }
 }
-
-
-
 </style>
