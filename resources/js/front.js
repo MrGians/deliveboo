527a78a53cf9c8e3882e530d5651771a40866811
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-10-28 12:52:14 +0200

    imported vuex store in app

--- resources/js/front.js
+++ resources/js/front.js
@@ -4,9 +4,11 @@ window.Vue = require("vue");
 
 import App from "./components/App.vue";
 import router from "./router.js";
+import store from './vuex.js';
 
 const app = new Vue({
     el: "#root",
     render: (h) => h(App),
     router,
+    store: new Vuex.Store(store),
 });
