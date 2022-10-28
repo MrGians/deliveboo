668d1f38ab40705b28b8aa091fbde423fb021683
MrGians <gianlumura97@gmail.com>
2022-10-28 15:24:50 +0200

    rebuilded BaseCard component

--- resources/js/components/BaseCard.vue
+++ resources/js/components/BaseCard.vue
@@ -1,57 +1,92 @@
 <template>
     <div class="card">
-        <div class="card-image rounded-5">
+        <div class="card-header rounded-5">
             <img
-                src="/img/burger_sandwich_PNG4135.png"
-                alt="placeholder"
+                :src="'storage/' + item.logo"
+                :alt="item.name"
                 class="img-fluid"
             />
         </div>
-        <slot></slot>
+        <div class="card-body">
+            <h4 class="title text-center">
+                {{ item.name }}
+            </h4>
+            <div class="categories text-center rounded-5">
+                <span v-for="category in item.categories" :key="category.id"
+                    >{{ category.label }}
+                </span>
+            </div>
+            <ul class="list">
+                <li>
+                    <i class="fa-solid fa-location-dot"></i>
+                    <span>{{ item.address }}</span>
+                </li>
+                <li>
+                    <i class="fa-solid fa-star"></i>
+                    <span>Voto: <strong> 5.00</strong></span>
+                </li>
+                <li>
+                    <i class="fa-solid fa-money-check-dollar"></i>
+                    <span><strong>Consegna Gratis</strong></span>
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: "BaseCard",
+    props: {
+        item: Object,
+    },
 };
 </script>
 
 <style scoped lang="scss">
+@import "./../../sass/front.scss";
+
 .card {
-    margin-bottom: 1.5rem;
-    .card-image {
-        background-color: #ffbd42;
-        padding: 1rem 2rem;
+    background-color: $secondary;
+    border-radius: 30px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    .card-header {
+        overflow: hidden;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
     }
 
-    .img-fluid {
-        max-width: 100%;
-        height: auto;
-    }
+    .card-body {
+        flex-grow: 1;
+        padding: 1rem;
+        border: 4px solid $secondary;
+        border-top: none;
+        border-radius: 30px;
+        background-color: white;
+        & .title {
+            padding: 0.5rem;
+            color: $tertiary;
+        }
 
-    ul {
-        list-style-position: inside;
-        list-style-type: none;
-        margin-top: 1rem;
-
-        & > li {
-            font-size: 0.7rem;
-            display: flex;
-            justify-content: space-between;
-
-            & > i {
-                color: #ffbd42;
-            }
-
-            & > span {
-                width: 85%;
-            }
+        .categories {
+            font-weight: bold;
+            border: 1px solid $quaternary;
         }
     }
 
-    .restaurant-name {
-        color: #fd5958;
+    .list {
+        margin-top: 1rem;
+
+        & > li {
+            display: flex;
+
+            & > i {
+                color: $secondary;
+                margin-right: 1rem;
+            }
+        }
     }
 }
 </style>
