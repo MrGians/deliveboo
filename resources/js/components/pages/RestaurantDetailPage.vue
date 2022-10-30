4ba0589c76394ccc3368380d5af68a5d3a7e5aeb
MrGians <gianlumura97@gmail.com>
2022-10-30 10:55:05 +0100

    fix & delete hardcoded stuff

--- resources/js/components/pages/RestaurantDetailPage.vue
+++ resources/js/components/pages/RestaurantDetailPage.vue
@@ -1,81 +1,122 @@
 <template>
-<section id="restaurant-detail">
-    <div class="container">
-        <!-- Card Restaurant -->
-        <div id="card-restaurant" class="row justify-content-center mb-5">
-            <div class="col-md-7">
-                <div class="card rounded-5 mt-5 mb-5 shadow">
-                    <div class="text-center">
-                        <div id="logo">
-                            <span class="text-logo">LOGO</span>
-                        </div>
-                        <div class="card-body">
-                            <ul class="information-restaurant">
-                                <li class="title-restaurant-card">Nome Ristorante</li>
-                                <li class="category">Categoria 1 Categoria 2</li>
-                                <li class="address">Via delle Belle Arti 2a, Bologna, 40162</li>
-                                <li class="option">Consegna gratis | Oridne minimo 10€</li>
-                                <li class="p-iva">P.IVA: 006354267</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Menu -->
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card rounded-5 mb-3 pb-5 shadow">
-                    <div class="row restaurant_dishes">
-                        <div class="col-3">
-                            <img src="../../../../public/img/piatto_1.jpg" alt="" class="restaurant_dishes_img">
-                        </div>
-                        <div class="col-6">
-                            <ul class="information-dishes">
-                                <li class="name_dishes">Nome piatto</li>
-                                <li class="category-dishes">Categoria 1 - Categoria 2</li>
-                                <li class="description-dishes">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error commodi omnis! Minus quae molestias</li>
-                                <li class="allergens-dishes"><i class="fa-solid fa-wheat-awn"></i>Allergeni</li>
-                                <li class="price-dishes"><i class="fa-solid fa-money-bill"></i>23.70</li>
-                            </ul>
-                        </div>
-                        <div class="col-3">
-                            <a href="#" class="add-cart">Aggiungi all'ordine</a>
-                        </div>
-                    </div>
-                    <hr>
-                    <div class="row restaurant_dishes">
-                        <div class="col-3">
-                            <img src="../../../../public/img/piatto_1.jpg" alt="" class="restaurant_dishes_img">
-                        </div>
-                        <div class="col-6">
-                            <ul class="information-dishes">
-                                <li class="name_dishes">Nome piatto</li>
-                                <li class="category-dishes">Categoria 1 - Categoria 2</li>
-                                <li class="description-dishes">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit error commodi omnis! Minus quae molestias</li>
-                                <li class="allergens-dishes"><i class="fa-solid fa-wheat-awn"></i>Allergeni</li>
-                                <li class="price-dishes"><i class="fa-solid fa-money-bill"></i>23.70</li>
-                            </ul>
-                        </div>
-                        <div class="col-3">
-                            <a href="#" class="add-cart">Aggiungi all'ordine</a>
+    <section id="restaurant-detail">
+        <div class="container">
+            <!-- Card Restaurant -->
+            <div id="card-restaurant" class="row justify-content-center mb-5">
+                <div class="col-md-7">
+                    <div class="card rounded-5 mt-5 mb-5 shadow">
+                        <div class="text-center">
+                            <div id="logo">
+                                <span class="text-logo">LOGO</span>
+                            </div>
+                            <div class="card-body">
+                                <ul class="information-restaurant">
+                                    <li class="title-restaurant-card">
+                                        Nome Ristorante
+                                    </li>
+                                    <li class="category">
+                                        Categoria 1 Categoria 2
+                                    </li>
+                                    <li class="address">
+                                        Via delle Belle Arti 2a, Bologna, 40162
+                                    </li>
+                                    <li class="option">
+                                        Consegna gratis | Oridne minimo 10€
+                                    </li>
+                                    <li class="p-iva">P.IVA: 006354267</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-</section>
+        <!-- Menu -->
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-12">
+                    <div class="card rounded-5 mb-3 pb-5 shadow">
+                        <div class="row restaurant_dishes">
+                            <div class="col-3">
+                                <img
+                                    src="storage/products_image/placeholder.png"
+                                    alt=""
+                                    class="restaurant_dishes_img"
+                                />
+                            </div>
+                            <div class="col-6">
+                                <ul class="information-dishes">
+                                    <li class="name_dishes">Nome piatto</li>
+                                    <li class="category-dishes">
+                                        Categoria 1 - Categoria 2
+                                    </li>
+                                    <li class="description-dishes">
+                                        Lorem ipsum, dolor sit amet consectetur
+                                        adipisicing elit. Sit error commodi
+                                        omnis! Minus quae molestias
+                                    </li>
+                                    <li class="allergens-dishes">
+                                        <i class="fa-solid fa-wheat-awn"></i
+                                        >Allergeni
+                                    </li>
+                                    <li class="price-dishes">
+                                        <i class="fa-solid fa-money-bill"></i
+                                        >23.70
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-3">
+                                <a href="#" class="add-cart"
+                                    >Aggiungi all'ordine</a
+                                >
+                            </div>
+                        </div>
+                        <hr />
+                        <div class="row restaurant_dishes">
+                            <div class="col-3">
+                                <img
+                                    src="storage/products_image/placeholder.png"
+                                    alt=""
+                                    class="restaurant_dishes_img"
+                                />
+                            </div>
+                            <div class="col-6">
+                                <ul class="information-dishes">
+                                    <li class="name_dishes">Nome piatto</li>
+                                    <li class="category-dishes">
+                                        Categoria 1 - Categoria 2
+                                    </li>
+                                    <li class="description-dishes">
+                                        Lorem ipsum, dolor sit amet consectetur
+                                        adipisicing elit. Sit error commodi
+                                        omnis! Minus quae molestias
+                                    </li>
+                                    <li class="allergens-dishes">
+                                        <i class="fa-solid fa-wheat-awn"></i
+                                        >Allergeni
+                                    </li>
+                                    <li class="price-dishes">
+                                        <i class="fa-solid fa-money-bill"></i
+                                        >23.70
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="col-3">
+                                <a href="#" class="add-cart"
+                                    >Aggiungi all'ordine</a
+                                >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style scoped lang="scss">
@@ -89,14 +130,14 @@ export default {
     margin-left: -15px;
 }
 
-.justify-content-center{
+.justify-content-center {
     justify-content: center;
 }
 
-.mb-5{
-    margin-bottom: 3rem;  
+.mb-5 {
+    margin-bottom: 3rem;
 }
-.mb-3{
+.mb-3 {
     margin-bottom: 1rem;
 }
 .col-3 {
@@ -111,55 +152,54 @@ export default {
     padding: 0px 10px;
 }
 .col-md-7 {
-        flex: 0 0 58.33333333%;
-        max-width: 58.33333333%;
+    flex: 0 0 58.33333333%;
+    max-width: 58.33333333%;
 }
 .col-md-12 {
-        flex: 0 0 100%;
-        max-width: 100%;
+    flex: 0 0 100%;
+    max-width: 100%;
 }
 
 .card {
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  min-width: 0;
-  word-wrap: break-word;
-  background-color: #fff;
-  background-clip: border-box;
-  border: 1px solid rgba(0, 0, 0, 0.125);
-  border-radius: 0.25rem;
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    min-width: 0;
+    word-wrap: break-word;
+    background-color: #fff;
+    background-clip: border-box;
+    border: 1px solid rgba(0, 0, 0, 0.125);
+    border-radius: 0.25rem;
 }
 
 .rounded-5 {
-  border-radius: 50px;
+    border-radius: 50px;
 }
 
-.mt-5{
+.mt-5 {
     margin-top: 3rem;
 }
 
-.pb-5{
-  padding-bottom: 3rem;
+.pb-5 {
+    padding-bottom: 3rem;
 }
 /* UTILS */
-#logo{
+#logo {
     position: absolute;
     top: -10px;
     left: 50%;
     transform: translate(-50%);
 }
 
-
 /* CARD RESTAURANT */
-#card-restaurant{
+#card-restaurant {
     margin-bottom: 100px;
 }
 
-.card{
+.card {
     background-color: #fff;
 }
-.text-logo{
+.text-logo {
     border-radius: 50%;
     background-color: #2fb871;
     padding: 20px;
@@ -167,35 +207,35 @@ export default {
     color: #fff;
 }
 
-ul.information-restaurant{
+ul.information-restaurant {
     padding-top: 50px;
 }
 
-li{
+li {
     list-style-type: none;
     font-size: 14px;
     padding-bottom: 20px;
 }
 
-.title-restaurant-card{
+.title-restaurant-card {
     font-size: 30px;
     color: #fc5958;
     font-weight: bold;
     padding-bottom: 30px;
 }
 
-.category{
+.category {
     font-size: 16px;
     font-weight: bold;
 }
 
-.option{
+.option {
     font-weight: bold;
     padding-bottom: 30px;
     font-size: 12px;
 }
 
-.p-iva{
+.p-iva {
     padding-bottom: 30px;
     font-size: 12px;
     color: #666;
@@ -203,44 +243,44 @@ li{
 /* CARD RESTAURANT */
 
 /* MENU */
-.restaurant_dishes{
+.restaurant_dishes {
     padding: 50px 20px;
     align-items: center;
 }
-.restaurant_dishes_img{
+.restaurant_dishes_img {
     max-width: 100%;
 }
 
-.name_dishes{
+.name_dishes {
     font-size: 20px;
     color: #fc5958;
     font-weight: bold;
     padding-bottom: 10px;
 }
 
-.category-dishes{
+.category-dishes {
     font-weight: bold;
 }
 
-.description-dishes{
+.description-dishes {
     padding-bottom: 25px;
 }
 
-.allergens-dishes{
+.allergens-dishes {
     padding-bottom: 5px;
 }
 
-.fa-solid{ 
+.fa-solid {
     color: #ffbd42;
     margin-right: 10px;
 }
 
-.price-dishes{
+.price-dishes {
     font-weight: bold;
     font-size: 17px;
 }
 
-.add-cart{
+.add-cart {
     padding: 40px 25px;
     background-color: #ffbd42;
     border-radius: 30px;
@@ -271,6 +311,5 @@ li{
         max-width: 1140px;
     }
 }
-
-</style>>
-
+</style>
+>
