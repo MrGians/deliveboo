0311932817116e7762150dfec96daba8b900ffd3
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-10-31 12:34:38 +0100

    added persistent cart feature

--- resources/js/vuex.js
+++ resources/js/vuex.js
@@ -7,10 +7,13 @@ window.Vuex = Vuex;
 Vue.use(Vuex);
 
 
+let cart = window.localStorage.getItem('cart');
+let cartCount = window.localStorage.getItem('cartCount');
+
 let store = {
     state: {
-        cart: [],
-        cartCount: 0,
+        cart: cart ? JSON.parse(cart) : [],
+        cartCount: cartCount ? parseInt(cartCount) : 0,
     },
     mutations: {
         addToCart(state, item) {
@@ -27,6 +30,7 @@ let store = {
             }
 
             state.cartCount++;
+            this.commit('saveCart');
         },
         removeFromCart(state, item) {
             let index = state.cart.indexOf(item);
@@ -37,6 +41,42 @@ let store = {
 
                 state.cart.splice(index, 1);
             }
+            this.commit('saveCart');
+        },
+        IncrementButton(state, item) {
+            let index = state.cart.indexOf(item);
+
+            let product = state.cart[index];
+
+            product.quantity++;
+            product.totalPrice = product.quantity * product.price;
+            state.cartCount++;
+
+            this.commit('saveCart');
+        },
+        DecrementButton(state, item) {
+            let index = state.cart.indexOf(item);
+
+            let product = state.cart[index];
+
+            product.quantity--;
+            product.totalPrice -= product.price;
+
+            if (!(product.quantity < 1)) {
+                state.cartCount--;
+
+            }
+
+            if (product.quantity < 1) {
+                product.quantity = 1;
+                product.totalPrice = product.price;
+            }
+            this.commit('saveCart');
+
+        },
+        saveCart(state) {
+            window.localStorage.setItem('cart', JSON.stringify(state.cart));
+            window.localStorage.setItem('cartCount', state.cartCount);
         }
     }
 };
