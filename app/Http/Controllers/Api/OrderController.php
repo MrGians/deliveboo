11cde3dc0ef3b7a5ace2f92037434732c5907f06
MrGians <gianlumura97@gmail.com>
2022-10-31 12:25:32 +0100

    add function to make payment in OrderController

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -24,4 +24,53 @@ class OrderController extends Controller
 
         return response()->json($data, 200);
     }
+
+    public function makePayment(Request $request)
+    {
+        // Validating payment request
+        $request->validate([
+            'token' => 'required',
+            'amount' => 'required|numeric|min:0.01',
+        ], [
+            'token.required' => 'Il Token è obbligatorio',
+            'amount.required' => 'Il totale è obbligatorio',
+            'amount.numeric' => 'Il totale deve essere un numero',
+            'amount.min' => 'Il totale non può essere inferiore a :min',
+        ]);
+
+        // Config Braintree Gateway
+        $gateway = new Gateway([
+            'environment' => 'sandbox',
+            'merchantId' => 'v3t2hg846dk826w5',
+            'publicKey' => 'njvyr2jwfwgqwdmg',
+            'privateKey' => '0ede10ba7ddefe1dcdeebf320b5157bc'
+
+        ]);
+
+        // Make a Transaction
+        $result = $gateway->transaction()->sale([
+            'amount' => $request->amount,
+            'paymentMethodNonce' => $request->token,
+            'options' => [
+            'submitForSettlement' => true,
+            ],
+        ]);
+
+        // Manages transaction's response
+        if($result->success){
+            $data = [
+                'success' => true,
+                'message' => 'Transazione eseguita con successo'
+            ];
+
+            return response()->json($data, 200);
+        } else {
+            $data = [
+                'success' => false,
+                'message' => 'Transazione Fallita'
+            ];
+            
+            return response()->json($data, 401);
+        }
+    }
 }
