71260642e52ab8ce8354a53de230f27ff16f900e
liviaAugusta <liviamattoni@gmail.com>
2022-11-01 23:21:32 +0100

    Added Not found page

--- resources/js/components/pages/NotFoundPage.vue
+++ resources/js/components/pages/NotFoundPage.vue
@@ -1,9 +1,70 @@
 <template>
-    <p>Sono il Not Found</p>
+    <div id="not-found-page">
+        <header>
+            <div id="card-container" class="not-fpund-card shadow text-center">
+                <h3>Ops, pagina non trovata</h3>
+                <p>Sembra che la pagina che cercavi non esista. Torna alla home per scegliere fra Fortunatamente,
+                    però, abbiamomigliaia di ristoranti pronti a consegnare a casa tua!</p>
+                <button type="submit" form="nameform" value="Home" class="home-btn">Home</button>
+            </div>
+        </header>
+    </div>
 </template>
 
 <script>
+import BaseHeader from '../BaseHeader.vue';
+
 export default {
     name: "App",
+    components: {
+        BaseHeader
+    },
 };
 </script>
+
+<style scoped lang="scss">
+@import "./../../../sass/front.scss";
+
+#title,
+h3 {
+    color: $tertiary;
+    font-size: 2rem;
+}
+
+#not-found-page {
+    position: relative;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .not-fpund-card {
+        width: 40em;
+        background-color: $body-bg;
+        padding: 3.5em;
+        border-radius: 30px;
+    }
+    
+    p {
+        margin: 2rem;
+    }
+
+    .home-btn {
+        display: inline-block;
+        padding: 0.6rem 1.2rem;
+        border: 2px solid $secondary;
+        background-color: $secondary;
+        color: $body-bg;
+        font-weight: 700;
+        border-radius: 30px;
+        margin-left: 1rem;
+        transition: all 0.35s;
+
+        &:hover {
+            background-color: $tertiary;
+            color: $body-bg;
+            border: 2px solid $tertiary;
+        }
+    }
+}
+</style>
