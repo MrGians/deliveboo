905696a754cefee2b1a8a508cbb8fa346bbc39ec
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-11-01 12:23:45 +0100

    added api call

--- resources/js/components/pages/RestaurantDetailPage.vue
+++ resources/js/components/pages/RestaurantDetailPage.vue
@@ -64,65 +64,24 @@ export default {
     name: 'RestaurantDetailPage',
     data() {
         return {
-            items: [
-                {
-                    id: 1,
-                    title: 'Children of Bodom - Hatebreeder',
-                    price: 9.99
-                },
-                {
-                    id: 2,
-                    title: 'Emperor - Anthems to the Welkin at Dusk',
-                    price: 6.66
-                },
-                {
-                    id: 3,
-                    title: 'Epica - The Quantum Enigma',
-                    price: 15.99
-                },
-                {
-                    id: 4,
-                    title: 'Chthonic - Takasago Army',
-                    price: 14.00
-                },
-                {
-                    id: 5,
-                    title: 'Silencer - Death - Pierce Me',
-                    price: 1.20
-                },
-                {
-                    id: 6,
-                    title: 'My Dying Bride - 34.788%... Complete',
-                    price: 10.00
-                },
-                {
-                    id: 7,
-                    title: 'Shape of Despair - Shades of',
-                    price: 7.80
-                },
-                {
-                    id: 8,
-                    title: 'Ne Obliviscaris - Portal of I',
-                    price: 11.30
-                },
-                {
-                    id: 9,
-                    title: 'Protest the Hero - Fortress',
-                    price: 5.55
-                },
-                {
-                    id: 10,
-                    title: 'Dark Lunacy - Devoid',
-                    price: 6.00
-                },
-            ]
+            detailRestaurant: [],
         }
     },
     methods: {
         addToCart(item) {
             this.$store.commit('addToCart', item);
+        },
+        fetchRestaurant() {
+            axios.get('http://127.0.0.1:8000/api/restaurants/' + this.$route.params.id)
+            .then((res) => {
+                this.detailRestaurant = res.data;
+            });
         }
-    }
+    },
+    mounted() {
+        this.fetchRestaurant()
+    },
+
 }
 </script>
 
