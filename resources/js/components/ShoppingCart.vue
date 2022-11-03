a3e777d71db95c0e03ceeb25d615558d265b2f04
GiuseppeBergamo <giuseppe.bergamo67@gmail.com>
2022-11-03 22:08:50 +0100

    fixed ShoppingCart style

--- resources/js/components/ShoppingCart.vue
+++ resources/js/components/ShoppingCart.vue
@@ -1,36 +1,43 @@
 <template>
     <div class="card rounded-5 mb-3 pb-5 shadow">
-        <div>
-            <h1>
-                Cart ({{ $store.state.cartCount }})
+        <div class="text-center">
+            <h1 class="mb-5 text-tertiary">
+                Carrello
             </h1>
         </div>
         <div v-if="$store.state.cart.length > 0">
-            <div v-for="item in $store.state.cart" :key="item.id">
-                <div class="col-6">
-                    {{ item.name }}
+            <div v-for="item in $store.state.cart" :key="item.id" id="cart-product" class="text-center">
+                <div class="mb-3">
+                    <strong>{{ item.name }}</strong>
                 </div>
-                <div class="col-3">
-                    <button @click="DecrementButton(item)">
+                <div class="mb-3">
+                    <button @click="DecrementButton(item)" class="button button-increment">
                         -
                     </button>
-                    x{{ item.quantity }}
-                    <button @click="IncrementButton(item)">
+                    <strong>{{ item.quantity }}</strong>
+                    <button @click="IncrementButton(item)" class="button button-increment">
                         +
                     </button>
                 </div>
-                <div class="col-3">
+                <div class="mb-3">
                     <!-- {{ item.totalPrice.toFixed(2) }} -->
-                    <button @click.prevent="removeFromCart(item)">
-                        elimina dal carrello
+                    <button @click.prevent="removeFromCart(item)" class="button button-remove">
+                        Elimina
                     </button>
                 </div>
-                <hr>
+                <div class="item-bar-container mb-5">
+                    <div class="item-bar">
+
+                    </div>
+                </div>
             </div>
-            <div>
-                Totale: {{ totalPrice }}
+            <div class="text-center">
+                <div class="mb-3">
+                    <strong>Totale:</strong>  
+                    <span>{{ totalPrice }}</span>
+                </div>
                 <div>
-                    <router-link :to="{ name: 'payment', params: { cart: this.$store.state.cart } }"><button>Prosegui con l'ordine</button></router-link>
+                    <router-link :to="{ name: 'payment', params: { cart: this.$store.state.cart } }"><button class="button button-order">Prosegui con l'ordine</button></router-link>
                 </div>
             </div>
         </div>
@@ -72,7 +79,74 @@ export default {
 @import './../../sass/front.scss';
 
 .card {
+    
     padding: 2rem;
+}
+
+.mb-3 {
+    margin-bottom: 0.5rem;
+}
+
+.mb-5 {
+    margin-bottom: 1.5rem;
+}
+
+.text-tertiary {
+    color: $tertiary;
+    font-size: 30px;
+}
+
+.item-bar-container {
+    padding: 0 50px;
+}
+.item-bar {
+    border-top: 3px solid $secondary;
+    border-radius: 5px;
+}
+
+.button {
+    display: inline-block;
+    padding: 0.4rem 0.6rem;
+    border: 2px solid $tertiary;
+    background-color: $tertiary;
+    color: white;
+    border-radius: 30px;
+    transition: all 0.35s;
+    cursor: pointer;
+
+    &:hover {
+        background-color: white;
+        color: $tertiary;
+    }
+}
+
+.button-remove {
+    border: 2px solid $tertiary;
+    background-color: $tertiary;
+    &:hover {
+        background-color: white;
+        color: $tertiary;
+    }
+}
+
+.button-increment{
+    border: 2px solid $secondary;
+    background-color: $secondary;
+    border-radius: 5px;
+    padding: 0.1rem 0.3rem;
+    &:hover {
+        background-color: white;
+        color: $secondary;
+    }
+}
+
+.button-order {
+    border: 2px solid $quaternary;
+    background-color: $quaternary;
+    &:hover {
+        background-color: white;
+        color: $quaternary;
+    }
 }
 
 </style>
