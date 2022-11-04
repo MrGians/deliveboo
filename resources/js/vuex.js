97aed120020b5aefa524d6a69ec257dcabd99442
MrGians <gianlumura97@gmail.com>
2022-11-04 16:44:44 +0100

    fix vuex cart

--- resources/js/vuex.js
+++ resources/js/vuex.js
@@ -1,15 +1,14 @@
-import { forEach } from 'lodash';
-import Vue from 'vue';
+import { forEach } from "lodash";
+import Vue from "vue";
 
-import Vuex from 'vuex';
+import Vuex from "vuex";
 
 window.Vuex = Vuex;
 
 Vue.use(Vuex);
 
-
-let cart = window.localStorage.getItem('cart');
-let cartCount = window.localStorage.getItem('cartCount');
+let cart = window.localStorage.getItem("cart");
+let cartCount = window.localStorage.getItem("cartCount");
 
 let store = {
     state: {
@@ -17,26 +16,6 @@ let store = {
         cartCount: cartCount ? parseInt(cartCount) : 0,
     },
     mutations: {
-        /*addToCart(state, item) {
-            let found = state.cart.find(product => product.id == item.id);
-
-            
-            if (found) {
-                found.quantity++;
-                found.totalPrice = found.quantity * found.price;
-            } else {
-                state.cart.push(item);
-
-                Vue.set(item, 'quantity', 1);
-                Vue.set(item, 'totalPrice', item.price);
-
-            }
-
-            state.cartCount++;
-
-            this.commit('saveCart');
-
-        },*/
         removeFromCart(state, item) {
             let index = state.cart.indexOf(item);
 
@@ -46,7 +25,7 @@ let store = {
 
                 state.cart.splice(index, 1);
             }
-            this.commit('saveCart');
+            this.commit("saveCart");
         },
         IncrementButton(state, item) {
             let index = state.cart.indexOf(item);
@@ -57,7 +36,7 @@ let store = {
             product.totalPrice = product.quantity * product.price;
             state.cartCount++;
 
-            this.commit('saveCart');
+            this.commit("saveCart");
         },
         DecrementButton(state, item) {
             let index = state.cart.indexOf(item);
@@ -69,29 +48,19 @@ let store = {
 
             if (!(product.quantity < 1)) {
                 state.cartCount--;
-
             }
 
             if (product.quantity < 1) {
                 product.quantity = 1;
                 product.totalPrice = product.price;
             }
-            this.commit('saveCart');
-
+            this.commit("saveCart");
         },
         saveCart(state) {
-            window.localStorage.setItem('cart', JSON.stringify(state.cart));
-            window.localStorage.setItem('cartCount', state.cartCount);
+            window.localStorage.setItem("cart", JSON.stringify(state.cart));
+            window.localStorage.setItem("cartCount", state.cartCount);
         },
-        /*resetCart(state) {
-            window.localStorage.removeItem('cart', JSON.stringify(this.$store.state.cart));
-            window.localStorage.removeItem('cartCount', this.$store.state.cartCount);
-            this.$store.state.cart = [];
-            this.$store.state.cartCount = 0;
-            this.$router.push({ name: "restaurants" });
-        }*/
-    }
+    },
 };
-
 
 export default store;
