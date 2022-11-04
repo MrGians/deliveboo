c674381c456a360591ab6832765338abed1b65c6
MrGians <gianlumura97@gmail.com>
2022-11-04 15:30:01 +0100

    added bootstrap icon instead font awesome

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -14,8 +14,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
         
 
-        {{-- Font Awesome --}}
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        {{-- Bootstrap icon --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/front.css') }}">
